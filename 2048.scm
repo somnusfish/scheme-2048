23d3d60872611116522a185b90db19f42a3b3d75
Lu Ma <somnusfish@gmail.com>
2014-08-16 21:19:40 +0800

    add chess box and win

--- 2048.scm
+++ 2048.scm
@@ -1,6 +1,4 @@
 #lang scheme
-(require scheme/match)
-(define test (list '(1 2 3 4 ) '(5 6 7 8 ) '(9 10 11 12 ) '(13 14 15 16 )))
 (define (make-chess-board)
   (define (make-element element num)
     (if (= num 0) 
@@ -23,6 +21,12 @@
                (list (* 2 (car lst)) 0)
                lst))
           (else lst)))
+  (define (mv0 lst)
+    (if (null? lst)
+        null
+        (if (zero? (car lst))
+            (append (mv0 (cdr lst)) (list 0))
+            (cons (car lst) (mv0 (cdr lst))))))
   (define (zero-list? lst)
     (andmap zero? lst))
   (define (shift lst)
@@ -32,7 +36,7 @@
             (append (shift (cdr lst))
                     (list 0))
             lst)))
-  (shift (left lst)))
+  (shift (left (mv0 lst))))
 
 (define (anticlc-rotate cb)
   (if (= 1 (length (car cb)))
@@ -40,6 +44,41 @@
       (append (anticlc-rotate (map cdr cb)) 
             (cons (map car cb) null))))
 
+(define (mv-left cb)
+  (map move-left-lst cb))
+(define (mv-right cb)
+  (anticlc-rotate
+   (anticlc-rotate
+    (mv-left 
+     (anticlc-rotate
+      (anticlc-rotate
+       cb))))))
+(define (mv-up cb)
+  (anticlc-rotate
+   (anticlc-rotate
+    (anticlc-rotate
+     (mv-left
+      (anticlc-rotate
+       cb))))))
+(define (mv-down cb)
+  (anticlc-rotate
+   (mv-left
+    (anticlc-rotate
+     (anticlc-rotate
+      (anticlc-rotate
+       cb))))))
+
+(define (win? cb)
+  (define (win-lst? lst)
+    (if (null? lst)
+        #f
+        (if (= 2048 (car lst))
+            #t
+            (win-lst? (cdr lst)))))
+  (if (null? cb)
+      #f
+      (if (win-lst? (car cb))
+          #t
+          (win? (cdr cb)))))
 
 
-(anticlc-rotate test)
